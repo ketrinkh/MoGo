401ba8019d5669468b4265b6c9b26f65f6eff61b
viktordulo <victordulo96@gmail.com>
2021-04-19 13:40:59 +0300

    animation added, html fixed

---
+++ script.js
@@ -0,0 +1,31 @@
+let element = document.body;
+
+let canvas = document.createElement('canvas');
+canvas.width = 1000;
+
+element.appendChild(canvas);
+
+let context = canvas.getContext('2d');
+
+let rectX = 0;
+let rectY = 50;
+
+function animate() {
+
+    context.fillStyle = 0xff0000;
+
+    if (rectX > 900) {
+        rectX -= 1;
+    }
+    if (rectX < 100) {
+        rectX += 1;
+    }
+    context.fillRect(rectX, rectY, 50, 50);
+    context.clearRect(rectX, rectY, 50, 50);
+    console.log('hello');
+
+    window.requestAnimationFrame(animate);
+}
+
+window.requestAnimationFrame(animate);
+window.cancelAnimationFrame(animate);
